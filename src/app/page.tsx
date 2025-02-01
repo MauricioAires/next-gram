d4938bdaeae9b954e9a022eed2517c1134f69563
MauricioAires <mauricioaires015@gmail.com>
2025-01-31 22:25:12 -0300

    :sparkles: create new schema with post likes and comments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,8 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
-      <Link href="/client">Página com client component</Link>
-      <Link href="/server">Página com server component</Link>
-      <Link href="/middleware">Página com middleware</Link>
-    </main>
+   <div>
+    Next gram
+   </div>
   );
 }
